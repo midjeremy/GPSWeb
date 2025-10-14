060f70a832098f6f73703ce413e55b6b123fcccf
miannate <k0dattpy@gmail.com>
2025-10-13 22:56:51 -0300

    vistas

--- GPSWebTesting/GPSWebTesting/urls.py
+++ GPSWebTesting/GPSWebTesting/urls.py
@@ -17,12 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Login.views import login
+from Tecnicos.views import empleado
 #from Supervisor.views import 
 #from Tecnicos.views import 
 
 urlpatterns = [
     path('', include('GPS.urls')),
     path('iniciar_sesion/', login, name="iniciar sesion"), #Pestaña de inicio de sesion
+    path('menu_empleado/', empleado, name = "Menu"), #MENÚ DEL EMPLEADO
     #path('registrar/', register, name="registrar"), #Añadir registro
     #path('/', , name=""), #Añadir Views de Supervisor
     #path('GPS/', include('GPS.urls'), name=""), #Añadir GPS.
